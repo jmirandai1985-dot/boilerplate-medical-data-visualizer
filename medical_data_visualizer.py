c4d871bc6cf10d1d75b67f56b146c3fb30d1eb25
jmirandai1985-dot <jmirandai1985@gmail.com>
2026-05-18 15:55:50 -0400

    Refactor medical data visualizer with new plots
    Signed-off-by: jmirandai1985-dot <jmirandai1985@gmail.com>

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,60 +1,83 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
+from pandas.plotting import register_matplotlib_converters
 
-# 1
-df = None
+register_matplotlib_converters()
 
-# 2
-df['overweight'] = None
+df = pd.read_csv(
+    "fcc-forum-pageviews.csv",
+    index_col="date",
+    parse_dates=True
+)
 
-# 3
-
-
-# 4
-def draw_cat_plot():
-    # 5
-    df_cat = None
+df = df[
+    (df["value"] >= df["value"].quantile(0.025)) &
+    (df["value"] <= df["value"].quantile(0.975))
+]
 
 
-    # 6
-    df_cat = None
-    
+def draw_line_plot():
+    df_line = df.copy()
 
-    # 7
+    fig, ax = plt.subplots(figsize=(16, 6))
+    ax.plot(df_line.index, df_line["value"], color="red")
 
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
 
-
-    # 8
-    fig = None
-
-
-    # 9
-    fig.savefig('catplot.png')
+    fig.savefig("line_plot.png")
     return fig
 
 
-# 10
-def draw_heat_map():
-    # 11
-    df_heat = None
+def draw_bar_plot():
+    df_bar = df.copy()
 
-    # 12
-    corr = None
+    df_bar["year"] = df_bar.index.year
+    df_bar["month"] = df_bar.index.month_name()
 
-    # 13
-    mask = None
+    df_bar = df_bar.groupby(["year", "month"])["value"].mean().unstack()
+
+    months_order = [
+        "January", "February", "March", "April",
+        "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    ]
+
+    df_bar = df_bar[months_order]
+
+    fig = df_bar.plot(kind="bar", figsize=(10, 8)).figure
+
+    plt.xlabel("Years")
+    plt.ylabel("Average Page Views")
+    plt.legend(title="Months")
+
+    fig.savefig("bar_plot.png")
+    return fig
 
 
+def draw_box_plot():
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
 
-    # 14
-    fig, ax = None
+    df_box["year"] = df_box["date"].dt.year
+    df_box["month"] = df_box["date"].dt.strftime("%b")
+    df_box["month_num"] = df_box["date"].dt.month
 
-    # 15
+    df_box = df_box.sort_values("month_num")
 
+    fig, axes = plt.subplots(1, 2, figsize=(20, 6))
 
+    sns.boxplot(x="year", y="value", data=df_box, ax=axes[0])
+    axes[0].set_title("Year-wise Box Plot (Trend)")
+    axes[0].set_xlabel("Year")
+    axes[0].set_ylabel("Page Views")
 
-    # 16
-    fig.savefig('heatmap.png')
+    sns.boxplot(x="month", y="value", data=df_box, ax=axes[1])
+    axes[1].set_title("Month-wise Box Plot (Seasonality)")
+    axes[1].set_xlabel("Month")
+    axes[1].set_ylabel("Page Views")
+
+    fig.savefig("box_plot.png")
     return fig
